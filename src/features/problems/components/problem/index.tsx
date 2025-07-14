d818c902b0ef381a6d91dd3ccea6c9e73e619cd7
suhyang.lee <suhyang.dev@gmail.com>
2025-07-14 19:16:12 +0900

    [Feat] react-hook-form field Array 적용 및 쿼리파람 추가

--- src/features/problems/components/problem/index.tsx
+++ src/features/problems/components/problem/index.tsx
@@ -1,21 +1,24 @@
-import React, { useEffect, useMemo } from "react";
-import { getProblemList } from "../../api/problem.api";
+import React, { useMemo } from "react";
 import CardItem from "../list/cardItem";
-import type { Problem } from "../../../../types/problem";
 
 import { ReactComponent as AddDeActiveCircleIcon } from "../../../../assets/icons/icon-add-circle-deactive.svg";
 import { ReactComponent as DeleteIcon } from "../../../../assets/icons/icon_delete.svg";
+import { useFieldArray, useFormContext } from "react-hook-form";
+import { Problem } from "../../../../types/problem";
+import { useSearchParams } from "react-router";
 
 const ProblemSection = () => {
-  const [problemList, setProblemList] = React.useState<Problem[]>([]);
+  const [, setSearchParams] = useSearchParams();
 
-  useEffect(() => {
-    (async function () {
-      const response = await getProblemList();
-      console.log(`[데이터 호출 테스트] ${response}`);
-      setProblemList(response.data);
-    })();
-  }, []);
+  const { control } = useFormContext<{
+    problems: Problem[];
+  }>();
+
+  const { fields, remove } = useFieldArray({
+    control,
+    name: "problems",
+    keyName: "itemId",
+  });
 
   const problemCounts = useMemo(() => {
     const counts = {
@@ -26,28 +29,45 @@ const ProblemSection = () => {
       5: 0,
     };
 
-    problemList.forEach(problem => {
+    fields.forEach(problem => {
       counts[problem.level]++;
     });
 
     return counts;
-  }, [problemList]);
+  }, [fields]);
+
+  const onFindSimilarProblem = (id: number) => {
+    setSearchParams(prev => {
+      prev.set("problemNum", `${id}`);
+      return prev;
+    });
+  };
+
+  const onDeleteProblem = (index: number) => {
+    remove(index);
+  };
 
   return (
     <div className="flex flex-col w-full lg:w-1/2 xl:flex-[89] bg-problem-right px-4 pt-4 rounded-xl overflow-hidden">
       <h2 className="body1-16-bold text-white mb-4">학습지 상세 편집</h2>
       <div className="h-full overflow-y-auto scrollbar-transparent">
         <ul className="flex flex-col gap-4">
-          {problemList.map(problem => (
-            <CardItem key={`${problem.id}`} cardType="problem" item={problem}>
+          {fields.map((problem, index) => (
+            <CardItem key={`${problem.itemId}-${index}`} cardType="problem" item={problem}>
               <>
                 <li>
-                  <button className="w-[65px] flex items-center gap-1 text-xs text-mono-959595-gray600">
+                  <button
+                    onClick={() => onFindSimilarProblem(problem.id)}
+                    className="w-[65px] flex items-center gap-1 text-xs text-mono-959595-gray600"
+                  >
                     <AddDeActiveCircleIcon className="w-4 h-4" /> 유사문제
                   </button>
                 </li>
                 <li>
-                  <button className="w-[43px] flex items-center gap-1 text-xs text-mono-959595-gray600">
+                  <button
+                    onClick={() => onDeleteProblem(index)}
+                    className="w-[43px] flex items-center gap-1 text-xs text-mono-959595-gray600"
+                  >
                     <DeleteIcon /> 삭제
                   </button>
                 </li>
@@ -61,7 +81,7 @@ const ProblemSection = () => {
           {`하${problemCounts["1"]} · 중하${problemCounts["2"]} · 중${problemCounts["3"]} · 상${problemCounts["4"]} · 최상${problemCounts["5"]}`}
         </span>
         <span className="w-[1px] h-4 mx-2 bg-white" />
-        <span className="body1-16-bold text-white">문제 수 {problemList.length || 0}개</span>
+        <span className="body1-16-bold text-white">문제 수 {fields.length || 0}개</span>
       </div>
     </div>
   );
--- src/pages/problem.tsx
+++ src/pages/problem.tsx
@@ -1,13 +1,36 @@
-import React from "react";
-import SimilarProblem from "../features/problems/components/similarProblem";
-import Problem from "../features/problems/components/problem";
+import React, { useEffect } from "react";
+import SimilarProblemSection from "../features/problems/components/similarProblem";
+import ProblemSection from "../features/problems/components/problem";
+import { FormProvider, useForm } from "react-hook-form";
+import { Problem } from "../types/problem";
+import { getProblemList } from "../features/problems/api/problem.api";
 
 function ProblemPage() {
+  const methods = useForm<{
+    problems: Problem[];
+  }>({
+    defaultValues: {
+      problems: [],
+    },
+  });
+
+  useEffect(() => {
+    (async function () {
+      const response = await getProblemList();
+      console.log(`[데이터 호출 테스트] ${response}`);
+      methods.reset({
+        problems: response.data,
+      });
+    })();
+  }, []);
+
   return (
-    <div className="fixed inset-0 flex gap-4 p-[14px]">
-      <SimilarProblem />
-      <Problem />
-    </div>
+    <FormProvider {...methods}>
+      <div className="fixed inset-0 flex gap-4 p-[14px]">
+        <SimilarProblemSection />
+        <ProblemSection />
+      </div>
+    </FormProvider>
   );
 }
 
