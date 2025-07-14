8935f7a5d8a48cbab0e6c515570428b4b7a5b772
suhyang.lee <suhyang.dev@gmail.com>
2025-07-14 18:01:16 +0900

    [Mod] 이미지 사이즈 고정값을 통해 비율 유지하며 줄어들 수 있도록 수정

--- src/features/problems/components/list/cardItem.tsx
+++ src/features/problems/components/list/cardItem.tsx
@@ -1,48 +1,44 @@
 import React from "react";
-import { ReactComponent as AddDeactiveCircleIcon } from "../../../../assets/icons/icon-add-circle-deactive.svg";
-import { ReactComponent as SwapIcon } from "../../../../assets/icons/icon_swap_horiz.svg";
+
 import { Problem } from "../../../../types/problem";
 import { getLevelColor, getLevelText } from "../../../../utils/problem.util";
 
 interface CardItemProps {
   item: Problem;
+  cardType: "problem" | "similarProblem";
   children?: React.ReactNode;
 }
 
-function CardItem({ item, children }: CardItemProps) {
+function CardItem({ item, cardType, children }: CardItemProps) {
   return (
     <li className="flex flex-col bg-white rounded-xl overflow-hidden shadow-[0_2px_6px_rgba(0,0,0,0.08)]">
       <div className="flex items-center justify-between bg-[#FAFAFA] p-2">
         <ul className="flex items-center flex-1 min-w-0 mr-4">
-          <li className="font-bold text-[18px] px-6">{item.id}</li>
+          <li className={`px-6 ${cardType === "problem" ? "H4-20-bold" : "H5-18-bold"}`}>{item.id}</li>
           <li className="text-sm truncate">{item.title}</li>
         </ul>
-        <ul className="flex gap-3 flex-shrink-0">
-          <li>
-            <button className="w-[43px] flex items-center gap-1 text-xs">
-              <SwapIcon className="w-4 h-4" /> 교체
-            </button>
-          </li>
-          <li>
-            <button className="w-[43px] flex items-center gap-1 text-xs">
-              <AddDeactiveCircleIcon /> 추가
-            </button>
-          </li>
-        </ul>
-        {children}
+        <ul className="flex gap-3 flex-shrink-0">{children}</ul>
       </div>
-      <div className="flex p-4">
+      <div className="flex p-4 gap-3.5">
         <ul className="flex flex-col gap-2">
           <li
-            className={`w-10 h-5 rounded text-xs flex items-center justify-center bg-[#FAFAFA] ${getLevelColor(item.level)}`}
+            className={`w-10 h-5 rounded text-xs flex items-center justify-center bg-mono-FAFAFA-gray100 ${getLevelColor(item.level)}`}
           >
             {getLevelText(item.level)}
           </li>
-          <li className="w-10 h-5 rounded text-xs flex items-center justify-center bg-[#FAFAFA]">72%</li>
-          <li className="w-10 h-5 rounded text-xs flex items-center justify-center bg-[#FAFAFA]">주관식</li>
+          <li className="w-10 h-5 rounded text-xs flex items-center justify-center bg-mono-FAFAFA-gray100 text-mono-707070-gray700">
+            {item.answerRate.toFixed(0)}%
+          </li>
+          <li className="w-10 h-5 rounded text-xs flex items-center justify-center bg-mono-FAFAFA-gray100 text-mono-959595-gray600">
+            주관식
+          </li>
         </ul>
-        <div>
-          <img src="" alt="" />
+        <div className="w-full">
+          <img
+            className="w-full lg:min-w-[271px] lg:max-w-[304px] xl:min-w-[341px] xl:max-w-[397px]"
+            src={item.problemImageUrl}
+            alt={item.title}
+          />
         </div>
       </div>
     </li>
