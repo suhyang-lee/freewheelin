7e9a247b161fb734ebf4697a4687dd00bf5464ed
suhyang.lee <suhyang.dev@gmail.com>
2025-07-14 13:00:32 +0900

    [Feat] 문제 페이지 기본적인 레이아웃 반영

--- src/pages/problem.tsx
+++ src/pages/problem.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Problem = () => {
-  return <div>problem</div>;
+  return (
+    <div className="flex">
+      <div className="w-full h-[340px] lg:w-1/2 xl:flex-[63] bg-red-50"></div>
+      <div className="w-full h-[340px] lg:w-1/2 xl:flex-[89] bg-amber-200"></div>
+    </div>
+  );
 };
 
 export default Problem;
