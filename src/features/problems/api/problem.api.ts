f864ab96e893fd012cc7c9d0ebbfb4f05c60e8ba
suhyang.lee <suhyang.dev@gmail.com>
2025-07-15 10:56:42 +0900

    [Fix] 유사 문제 조회 API 수정

--- src/features/problems/api/problem.api.ts
+++ src/features/problems/api/problem.api.ts
@@ -14,8 +14,13 @@ export const getProblemList = () => {
  * 문제 리스트 아이디로 유사 문제 리스트 가져오기
  *
  * @param {number} problemId - 선택된 problem ID
+ * @param {string} excludedProblemIds - 현재 선택되지않은 IDs
  * @returns 유사 문제 리스트
  */
-export const getSimilarProblemList = (problemId: number) => {
-  return ApiClient.get<Array<Problem>>(`/problems/${problemId}/similarity`);
+export const getSimilarProblemList = (problemId: number, excludedProblemIds: Array<number>) => {
+  return ApiClient.get<Array<Problem>>(`/problems/${problemId}/similarity`, {
+    params: {
+      excludedProblemIds: excludedProblemIds.join(","),
+    },
+  });
 };
