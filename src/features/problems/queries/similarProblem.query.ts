0f50d46d7c68aaaad4b4f53c5fd915ff8cb54af4
suhyang.lee <suhyang.dev@gmail.com>
2025-07-15 10:57:26 +0900

    [Fix] 유사 문제 쿼리 부분 예외 아이디 추가하여 수정

--- src/features/problems/queries/similarProblem.query.ts
+++ src/features/problems/queries/similarProblem.query.ts
@@ -3,13 +3,13 @@ import { getSimilarProblemList } from "../api/problem.api";
 
 const similarProblemQuery = {
   key: "similarProblem",
-  getSimilarProblemList: (id: number) => {
+  getSimilarProblemList: (id: number, excludedProblemIds: Array<number>) => {
     return useQuery({
-      queryKey: [similarProblemQuery.key, id],
+      queryKey: [similarProblemQuery.key, id, ...excludedProblemIds],
       retry: false,
       enabled: id !== -1 && !isNaN(id),
       queryFn: async () => {
-        const res = await getSimilarProblemList(id);
+        const res = await getSimilarProblemList(id, excludedProblemIds);
         return res.data || [];
       },
       throwOnError: true,
